fc2802add129a4bffca00dc5b9a67f6f148bde31
Swapnil Ranawat <sanjaylucky931@gmail.com>
2026-09-04 12:15:18 +0530

    Add initial C programs

---
+++ 01-BASICS/helo_world.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+    printf("Hello WORLD");
+    return 0;
+}
+
---
+++ 01-BASICS/multiple_lines.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+    printf("Hello!");
+    printf("My name is Swapnil Ranawat.");
+    printf("I am learning C from scratch. ");
+    return 0;
+}
