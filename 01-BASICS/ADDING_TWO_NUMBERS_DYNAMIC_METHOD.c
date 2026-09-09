fd807f011f034a988d38271ee86a39b71642b8e7
Swapnil Ranawat <sanjaylucky931@gmail.com>
2026-09-09 21:23:24 +0530

    added programs to add two numbers in static and dynamic method

---
+++ 01-BASICS/ADDING_TWO_NUMBERS_DYNAMIC_METHOD.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter the two no to be added");
+    scanf("%d",&a);
+    scanf("%d",&b);
+    c=a+b;
+    printf("%d",c);
+    return 0;
+}
